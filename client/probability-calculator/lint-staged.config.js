3045da3f4d53643fa213d5ac2de2e596c7788e6f
Ben Blackmore <ben.blackmore@greaterheights.net>
2025-04-06 19:55:02 +0100

    chore: remove lint staged

--- client/probability-calculator/lint-staged.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  '!(*.{js,jsx,ts,tsx})': ['npm validate:staged:format'],
-  '*.{js,jsx,ts,tsx}': [
-    'npm validate:staged:format',
-    'npm validate:staged:lint',
-  ],
-  '*.{ts,tsx}': [() => 'npm validate:staged:types'],
-};
